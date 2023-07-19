f3bc1fd9d1d6001feb9ec8b185e8f7edd6b5a44e
Faysal <98054543+CoderFaysal@users.noreply.github.com>
2023-07-19 09:17:31 +0600

    Add files via upload

---
+++ AdsUnit.java
@@ -0,0 +1,10 @@
+package com.coderfaysal.myshariatpur.Ads;
+
+public class AdsUnit {
+
+    public static String BANNER = "ca-app-pub-3940256099942544/6300978111";
+    public static String INTERSTITIAL = "ca-app-pub-3940256099942544/1033173712";
+
+    public static boolean isAds = true;
+
+}
